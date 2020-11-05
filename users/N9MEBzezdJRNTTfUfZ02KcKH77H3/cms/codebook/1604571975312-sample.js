a8f0b6f09aeedd2585a8fe6c2f2747089ebf63e2
undefined <undefined>
2020-11-05 18:06:31 +0530

    2 modified commit!

---
+++ users/N9MEBzezdJRNTTfUfZ02KcKH77H3/cms/codebook/1604571975312-sample.js
@@ -0,0 +1,30 @@
+import { existsSync } from 'fs-extra'
+import simplegit from 'simple-git/promise'
+
+export class GitService {
+  private git: simplegit.SimpleGit
+  private readonly path: string
+  private readonly repo: string
+
+//TEst
+
+  constructor () {
+    this.path = process.env.GIT_PATH
+    this.repo = process.env.GIT_REPO
+
+    if (!existsSync(this.path)) {
+      simplegit().silent(true).clone(this.repo, this.path).catch()
+    }
+
+    this.git = simplegit(this.path).silent(true)
+  }
+
+  public async getLastVersion () {
+    await this.git.fetch()
+    return this.git.pull('origin', 'master')
+  }
+
+  public getPath (): string {
+    return this.path
+  }
+}
