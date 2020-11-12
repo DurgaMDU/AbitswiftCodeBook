518f71579fd035113b4136ba2a36675f98948b60
Gopinath <gopystar@gmail.com>
2020-11-12 12:35:10 +0000

    Code File Edited by Gopinath

--- users/N9MEBzezdJRNTTfUfZ02KcKH77H3/cms/codebook/1605184284017-index.js
+++ users/N9MEBzezdJRNTTfUfZ02KcKH77H3/cms/codebook/1605184284017-index.js
@@ -13,35 +13,4 @@ import firebaseApp from '~/plugins/firebase.js'
 db.settings(settings)*/
 
 
-const createStore = () => {
-return new Vuex.Store({
-  state: {
-	
-	//Verticaltab active
-	activetab: 0,
-	
-	//Page Loading
-	pageloading: false,  
-	loading: false,  
-	  
-    //Notify snackbar
-	snack : false,
-	snackColor : 'info',
-	snackText : 'Info Message',
-	
-	//Add response dialog
-    adddialog: false,
-    additem: {questionid: '', name: '',email:'',phonenumber:'',response:'This song is dedicated to ...', approve: 0, status: 1,platform:'website',messagetone:'',audio:0  },
-	
-	sidebar: true,
-	ip: 'DDD',
-	uid: '',
-    user: '',
-	userdet: {email:'',id:null},
-     
-});
-
-}
-
-
 export default createStore
