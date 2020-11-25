b974f3e4f852490ec135ac8ea14e741891c201c1
Gopinath <gopystar@gmail.com>
2020-11-25 09:25:22 +0530

    Create Jenkins jobs - codebook added by Gopinath

---
+++ users/N9MEBzezdJRNTTfUfZ02KcKH77H3/cms/codebook/sample.py
@@ -0,0 +1,9 @@
+# Taking kilometers input from the user
+kilometers = 5
+
+# conversion factor
+conv_fac = 0.621371
+
+# calculate miles
+miles = kilometers * conv_fac
+print(&quot;%0.2f kilometers is equal to %0.2f miles&quot; %(kilometers,miles))
